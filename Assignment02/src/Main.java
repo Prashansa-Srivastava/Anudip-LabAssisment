7b7931fddae2005d10256d8b7d91b315fea4b534
Prashansa-Srivastava <91961990+Prashansa-Srivastava@users.noreply.github.com>
2022-12-15 17:41:32 +0530

    Add files via upload

---
+++ Assignment02/src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		// instance of Employee class
+		Employee emp = new Employee("Shashwat",20,"8********9","Noida sector 63",567845.7,"IT");
+		// instance of Manager class
+		Manager mng = new Manager("Prashansa",22,"9*********0","New Delhi",800234.66,"Marketing");
+		// now print the employee info
+		emp.printName();
+		emp.printAge();
+		emp.printPhoneNumber();
+		emp.printAddress();
+		emp.printSalary();
+		
+		//now print Manager info
+		mng.printName();
+		mng.printAge();
+		mng.printPhoneNumber();
+		mng.printAddress();
+		mng.printSalary();
+		
+	}
+
+
+}
